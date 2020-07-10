dc83fdf3a07e48ae24e5a86edd5f08589f77420d
LumbermanOne <seangeorge117@gmail.com>
2020-07-09 20:48:39 -0500

    Updated python main

--- py/main.py
+++ py/main.py
@@ -7,7 +7,6 @@ def main():
     framenum   = 0
     framerate  = 60
     frametotal = 240
-    images     = [] # Create array of images {image,x,y,rgb}
 
     
     # Iterators
@@ -19,18 +18,22 @@ def main():
     # Connect to camera
     camera = picamera.PiCamera()
     # camera.resolution = (640, 480)
-    camera.resolution = (64, 64)
+    camera.resolution = (640, 480)
     camera.brightness = 100
     camera.framerate = framerate
     
+    # Create array of images {image,x,y,rgb}
+    images     = numpy.empty((240, 640, 480, 3), dtype=numpy.uint8)
+    # Create temp array to store images 
+    #img = numpy.zeros((64,64,3))
     
-    
+        
     while 1: 
         for i in range(i, i+10): # ( ; i < (j+10); i++)
-            # img = picamera.array.PiRGBArray(camera)
-            # camera.capture(img, 'rgb')
-            # images.append(img)
-            # img.truncate(0)
+            img = numpy.zeros((640, 480, 3), dtype=numpy.uint8)
+            camera.capture(img, 'rgb')
+            images[i] = img
+            #img.truncate(0)
             framenum += 1
             # take and store image
             # get rgb signal channels
@@ -43,34 +46,31 @@ def main():
         
 
         print(framenum)
-        if framenum == frametotal:
+        if framenum == 10: #frametotal:
             break
-
-
 
     ### TESTING ###  ### TESTING ### 
 
     # image = camera.capture(output, 'rgb')
-    img = numpy.zeros((64,64,3))
-    img = picamera.array.PiRGBArray(camera)
-    camera.capture(img, 'rgb')
-    numpy.append(images,img, 0)
+
+    #camera.capture(img, 'rgb')
+    #numpy.append(images,img)
     # igg2.append(img)
-    img.truncate(0)
-    camera.capture(img, 'rgb')
-    images.append(img)
+    #img.truncate(0)
+    #camera.capture(img, 'rgb')
+    #images.append(img)
     # camera.capture_sequence([images for i in range(10)])
 
-    print(igg2)
-    print(images)
+    #print(igg2)
+    #print(images)
 
-    if images[0] == images[1]:
-        print("Equal")
-    else:
-        print("Not Equal")
+    #if images[0] == images[1]:
+    #    print("Equal")
+    #else:
+    #    print("Not Equal")
 
     # framenum, image x, image y, rgb
-    print('Captured Image[%d] %dx%dx%d ' % (framenum, img.array.shape[1], img.array.shape[0], img.array.shape[2]))
+    # print('Captured Image[%d] %dx%dx%d ' % (framenum, img.array.shape[1], img.array.shape[0], img.array.shape[2]))
 
     ### TESTING ###  ### TESTING ### 
 
